a9cf848a3c14e3242bccf4d35badee796f2f27fc
Dan Marsden <dan@danmarsden.com>
2014-10-31 14:33:38 +1300

    First version of simplecontact block (in development)

---
+++ lang/en/block_simplecontact.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_simplecontact', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    block_simplecontact
+ * @copyright  2014 onwards Dan Marsden (http://danmarsden.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['allowadditionalcssclasses'] = 'Allow additional CSS classes';
+$string['configallowadditionalcssclasses'] = 'Adds a configuration option to simple contact block instances allowing additional CSS classes to be set.';
+$string['configclasses'] = 'Additional CSS classes';
+$string['configclasses_help'] = 'The purpose of this configuration is to aid with theming by helping distinguish simple contact blocks from each other. Any CSS classes entered here (space delimited) will be appended to the block\'s default classes.';
+$string['configcontent'] = 'Content';
+$string['configdefaultemail'] = 'The email address used by default as the recipient of the contact form';
+$string['configtitle'] = 'Block title';
+$string['defaultemail'] = 'Default email address';
+$string['simplecontact:addinstance'] = 'Add a new simple contact block';
+$string['simplecontact:myaddinstance'] = 'Add a new simple contact block to My home';
+$string['leaveblanktohide'] = 'leave blank to hide the title';
+$string['newsimplecontactblock'] = '(new simple contact block)';
+$string['pluginname'] = 'Simple contact';
+$string['textheader'] = 'Header text';
+$string['configtextheader'] = 'The default text to show in the header of the form';
+$string['textfooter'] = 'Header text';
+$string['configtextfooter'] = 'The default text to show in the footer of the form';
